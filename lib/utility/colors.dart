b3847f98d3d14537c46dcef71f1969bbe4a6c189
Michael Angelo Cadavillo <michael.cadavillo@eigital.com>
2021-05-15 00:04:21 +0800

    Added Color theme

---
+++ lib/utility/colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const PrimaryColor = Color(0xFFEAF0FB);
+  static const DarkPrimaryColor = Color(0xFF212121);
+  static const AccentColor = Color(0xFF0083B7);
+  static const LightAccentColor = Color(0xFF00AEF0);
+
+  static const ErrorText = Color(0xFFD0021B);
+}
